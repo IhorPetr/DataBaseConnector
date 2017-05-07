c58c8a57ede43031a42e937af4c97157184fc110
IhorPetr <vep2014@ukr.net>
2017-05-07 16:53:24 +0300

    Add validator

--- DataBaseMigrator/DataBaseMigrator/Controllers/AuthenticationController.cs
+++ DataBaseMigrator/DataBaseMigrator/Controllers/AuthenticationController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataBaseMigrator.Models;
 
 namespace DataBaseMigrator.Controllers
 {
     public class AuthenticationController : Controller
     {
         // GET: Authentication
-        public ViewResult Enter()
+        [HttpGet]
+        public ActionResult Enter()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Enter(EnterStatus f)
+        {
+            if(ModelState.IsValid)
+            {
+                return View(f);
+            }
+            return View(f);
         }
     }
 }
---
+++ DataBaseMigrator/DataBaseMigrator/Models/EnterStatus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace DataBaseMigrator.Models
+{
+    public class EnterStatus
+    {
+        [Required]
+        [Display(Name = "Сервер:")]
+        public string ServerName1 { get; set; }
+        [Required]
+        [Display(Name = "Назва БД:")]
+        public string BDName1 { get; set; }
+        [Required]
+        [Display(Name = "Логін:")]
+        public string Login1 { get; set; }
+        [Required]
+        [Display(Name = "Пароль:")]
+        [DataType(DataType.Password)]
+        public string Password1 { get; set; }
+        [Required]
+        [Display(Name = "Сервер:")]
+        public string ServerName2 { get; set; }
+        [Required]
+        [Display(Name = "Назва БД:")]
+        public string BDName2 { get; set; }
+        [Required]
+        [Display(Name = "Логін:")]
+        public string Login2 { get; set; }
+        [Required]
+        [Display(Name = "Пароль:")]
+        [DataType(DataType.Password)]
+        public string Password2 { get; set; }
+
+    }
+}
