601297bea838cd2291b5a0a63a04524a4bd95310
Ihor <vep2014@ukr.net>
2017-11-18 08:47:07 +0200

    Improve algo

--- DataBaseMigrator/DataBaseMigrator/Infrastructure/MigratorCore.cs
+++ DataBaseMigrator/DataBaseMigrator/Infrastructure/MigratorCore.cs
@@ -15,13 +15,14 @@ namespace DataBaseMigrator.Infrastructure
         protected void DataCheck(ref DataTable campus, ref DataTable Vkd)
         {
             int _maximum = campus.Rows.Count == 0 ? 1 : campus.Rows.Cast<DataRow>().Select(p => Convert.ToInt32(p["eEmployees1Id"])).Max() + 1;
+            bool check = false;
 
-            var EmployeeIdlist = Vkd.AsEnumerable().Select(y =>new
+            var EmployeeIdlist = Vkd.AsEnumerable().Select(y => new
             {
-                EmpId=y["ID_employee"],
-                SubId=y["ID_Subdivision"]
+                EmpId = y["ID_employee"],
+                SubId = y["ID_Subdivision"]
             }).Distinct().ToList();
-           
+
             var vkdRows = Vkd.Rows.Cast<DataRow>().ToList();
             var campusRows = campus.Rows.Cast<DataRow>().ToList();
             for (int rowNumber = 0; rowNumber < EmployeeIdlist.Count; rowNumber++)
@@ -30,63 +31,124 @@ namespace DataBaseMigrator.Infrastructure
 
                 NLogCore.LogStatusAplication(String.Format("DataCheck : {0} of {1}", rowNumber, EmployeeIdlist.Count));
 
-                var gottenEmployes = vkdRows.Where(o => o["ID_employee"].ToString() == row.EmpId.ToString() 
+                var gottenEmployes = vkdRows.Where(o => o["ID_employee"].ToString() == row.EmpId.ToString()
                 && o["ID_Subdivision"].ToString() == row.SubId.ToString())
-                .OrderBy(e=>e["RowNumber"].ToString())
+                .OrderBy(e => e["RowNumber"].ToString())
                 .ToList();
-                var existingEmployes = campusRows.Where(t => t["ID_employee"].ToString() == row.EmpId.ToString() 
-                && t["ID_Subdivision"].ToString() == row.SubId.ToString() 
-                && t["vcChangeStatus"].ToString() != "Видалено")
+                var existingEmployes = campusRows.Where(t => t["ID_employee"].ToString() == row.EmpId.ToString()
+                && t["ID_Subdivision"].ToString() == row.SubId.ToString())
+                //  && t["vcChangeStatus"].ToString() != "Видалено")
                 .OrderBy(e => e["eEmployees1Id"].ToString())
                 .ToList();
 
                 if (gottenEmployes.Count == existingEmployes.Count)
                 {
-                    for (int i = 0; i < gottenEmployes.Count(); i++)
+                    var indexis = gottenEmployes.Count;
+                    for (int i = 0; i < indexis; i++)
                     {
-                        //if (existingEmployes[i]["vcChangeStatus"].ToString() != "Видалено")
-                        //{
+                        if (check)
+                        {
+                            i = 0;
+                            check = false;
+                        }
+                        if (existingEmployes[i]["ID_Duties"].ToString() == gottenEmployes[i]["ID_Duties"].ToString()
+                            && existingEmployes[i]["ID_Group"].ToString() == gottenEmployes[i]["ID_Group"].ToString()
+                            && existingEmployes[i]["IDEmploymentForm"].ToString() ==
+                            gottenEmployes[i]["IDEmploymentForm"].ToString())
+                        {
                             RowUpdate(existingEmployes[i], gottenEmployes[i]);
-                        //}
-                        //else
-                        //{
-                        //    var sourceRow = gottenEmployes[i];
-                        //    var newRow = campus.NewRow();
-                        //    CloneRow(sourceRow, _maximum, ref newRow);
-
-                        //    campus.Rows.Add(newRow);
-                        //    NLogCore.LogStatusAplication("Додано запис з ідентифікатором eEmployees1Id=" + _maximum.ToString());
-                        //    _maximum = _maximum + 1;
-                        //}
+                            existingEmployes.RemoveAt(i);
+                            gottenEmployes.RemoveAt(i);
+                            indexis -= 1;
+                            check = true;
+                        }
+                        else
+                        {
+                            var getSameEmployee = existingEmployes.Find(t =>
+                                t["ID_Duties"].ToString() == gottenEmployes[i]["ID_Duties"].ToString()
+                                && t["ID_Group"].ToString() == gottenEmployes[i]["ID_Group"].ToString()
+                                && t["IDEmploymentForm"].ToString() ==
+                                gottenEmployes[i]["IDEmploymentForm"].ToString());
+                            if (getSameEmployee != null)
+                            {
+                                var getSameEmployeeIndex = existingEmployes.FindIndex(t =>
+                                    t["ID_Duties"].ToString() == gottenEmployes[i]["ID_Duties"].ToString()
+                                    && t["ID_Group"].ToString() == gottenEmployes[i]["ID_Group"].ToString()
+                                    && t["IDEmploymentForm"].ToString() ==
+                                    gottenEmployes[i]["IDEmploymentForm"].ToString());
+                                RowUpdate(existingEmployes[getSameEmployeeIndex], gottenEmployes[i]);
+                                existingEmployes.RemoveAt(getSameEmployeeIndex);
+                                gottenEmployes.RemoveAt(i);
+                                indexis -= 1;
+                                check = true;
+                            }
+                        }
+                    }
+                    if (gottenEmployes.Count != 0)
+                    {
+                        for (int i = 0; i < gottenEmployes.Count; i++)
+                        {
+                            RowUpdate(existingEmployes[i], gottenEmployes[i]);
+                        }
                     }
                 }
 
                 if (gottenEmployes.Count > existingEmployes.Count)
                 {
-                    for (int i = 0; i < gottenEmployes.Count(); i++)
+                    int index = gottenEmployes.Count;
+                    for (int i = 0; i < index; i++)
                     {
-                        if (i<existingEmployes.Count)
+                        if (check)
                         {
-                            RowUpdate(existingEmployes[i], gottenEmployes[i]);
+                            i = 0;
+                            check = false;
                         }
-                        else
+                        var getSameEmployee = existingEmployes.Find(t =>
+                            t["ID_Duties"].ToString() == gottenEmployes[i]["ID_Duties"].ToString()
+                            && t["ID_Group"].ToString() == gottenEmployes[i]["ID_Group"].ToString()
+                            && t["IDEmploymentForm"].ToString() ==
+                            gottenEmployes[i]["IDEmploymentForm"].ToString());
+                        if (getSameEmployee != null)
                         {
-                            var sourceRow = gottenEmployes[i];
-                            var newRow = campus.NewRow();
-                            CloneRow(sourceRow, _maximum, ref newRow);
+                            var getSameEmployeeIndex = existingEmployes.FindIndex(t =>
+                                t["ID_Duties"].ToString() == gottenEmployes[i]["ID_Duties"].ToString()
+                                && t["ID_Group"].ToString() == gottenEmployes[i]["ID_Group"].ToString()
+                                && t["IDEmploymentForm"].ToString() ==
+                                gottenEmployes[i]["IDEmploymentForm"].ToString());
+                            RowUpdate(existingEmployes[getSameEmployeeIndex], gottenEmployes[i]);
+                            existingEmployes.RemoveAt(getSameEmployeeIndex);
+                            gottenEmployes.RemoveAt(i);
+                            index -= 1;
+                            check = true;
+                        }
+                    }
+                    if (gottenEmployes.Count() != 0)
+                    {
+                        for (int i = 0; i < gottenEmployes.Count(); i++)
+                        {
+                            if (i+1 < gottenEmployes.Count)
+                            {
+                                RowUpdate(existingEmployes[i], gottenEmployes[i]);
+                            }
+                            else
+                            {
+                                var sourceRow = gottenEmployes[i];
+                                var newRow = campus.NewRow();
+                                CloneRow(sourceRow, _maximum, ref newRow);
 
-                            campus.Rows.Add(newRow);
-                            NLogCore.LogStatusAplication("Додано запис з ідентифікатором eEmployees1Id=" + _maximum.ToString());
-                            _maximum = _maximum + 1;
+                                campus.Rows.Add(newRow);
+                                NLogCore.LogStatusAplication(
+                                    "Додано запис з ідентифікатором eEmployees1Id=" + _maximum.ToString());
+                                _maximum = _maximum + 1;
+                            }
                         }
                     }
                 }
 
                 if (gottenEmployes.Count < existingEmployes.Count)
                 {
-                    //int index1 = existingEmployes.Count;
-                    //int index2 = gottenEmployes.Count;
-                    //bool check = false;
+                    int index1 = existingEmployes.Count;
+                    int index2 = gottenEmployes.Count;
 
                     //for (int i = 0; i < index1; i++)
                     //{
@@ -109,56 +171,56 @@ namespace DataBaseMigrator.Infrastructure
                     //    }
                     //}
 
-                    //for (int i = 0; i < index1; i++)
-                    //{
-                    //for (int j = 0; j < index2; j++)
-                    //{
-                    //    if (check)
-                    //    {
-                    //         j = 0; check = false;
-                    //    }
-                    int index2 = 0;
-
-                    foreach (var gotEmployee in gottenEmployes.ToArray())
+                    for (int i = 0; i < index1; i++)
                     {
-                        if (existingEmployes.Any(r => r["ID_Duties"].ToString() == gotEmployee["ID_Duties"].ToString())
-                            && existingEmployes.Any(r => r["ID_Group"].ToString() == gotEmployee["ID_Group"].ToString())
-                            && existingEmployes.Any(r => r["IDEmploymentForm"].ToString() == gotEmployee["IDEmploymentForm"].ToString()))
+                        for (int j = 0; j < index2; j++)
                         {
-                            var getthis = existingEmployes.Find(r =>
-                                r["ID_Duties"].ToString() == gotEmployee["ID_Duties"].ToString() &&
-                                r["ID_Group"].ToString() == gotEmployee["ID_Group"].ToString() &&
-                                r["IDEmploymentForm"].ToString() == gotEmployee["IDEmploymentForm"].ToString());
-                            // var 
-                            var index1 = existingEmployes.FindIndex(r =>
-                                r["ID_Duties"].ToString() == gotEmployee["ID_Duties"].ToString() &&
-                                r["ID_Group"].ToString() == gotEmployee["ID_Group"].ToString() &&
-                                r["IDEmploymentForm"].ToString() == gotEmployee["IDEmploymentForm"].ToString());
-                            RowUpdate(getthis, gotEmployee);
-                            existingEmployes.RemoveAt(index1);
-                            gottenEmployes.RemoveAt(index2);
-                            //index1 = index1 - 1;
-                            //index2 = index2 - 1;
-                            //check = true;
-                        }
-                        index2++;
-                    }
-                
+                            if (check)
+                            {
+                                j = 0; i = 0; check = false;
+                            }
+                            // int index2 = 0;
 
-                   
-                   // }
+                            // foreach (var gotEmployee in gottenEmployes.ToList())
+                            // {
+                            if (existingEmployes[i]["ID_Duties"].ToString() == gottenEmployes[j]["ID_Duties"].ToString()
+                                && existingEmployes[i]["ID_Group"].ToString() == gottenEmployes[j]["ID_Group"].ToString()
+                                && existingEmployes[i]["IDEmploymentForm"].ToString() == gottenEmployes[j]["IDEmploymentForm"].ToString())
+                            {
+                                //var getthis = existingEmployes.Find(r =>
+                                //    r["ID_Duties"].ToString() == gottenEmployes[i]["ID_Duties"].ToString() &&
+                                //    r["ID_Group"].ToString() == gottenEmployes[i]["ID_Group"].ToString() &&
+                                //    r["IDEmploymentForm"].ToString() == gottenEmployes[i]["IDEmploymentForm"].ToString());
+                                //// var 
+                                //var index = existingEmployes.FindIndex(r =>
+                                //    r["ID_Duties"].ToString() == gottenEmployes[i]["ID_Duties"].ToString() &&
+                                //    r["ID_Group"].ToString() == gottenEmployes[i]["ID_Group"].ToString() &&
+                                //    r["IDEmploymentForm"].ToString() == gottenEmployes[i]["IDEmploymentForm"].ToString());
+                                RowUpdate(existingEmployes[i], gottenEmployes[j]);
+                                existingEmployes.RemoveAt(i);
+                                gottenEmployes.RemoveAt(j);
+                                index1 -= 1;
+                                index2 -= 1;
+                                check = true;
+                            }
+                            //   index2++;
+                        }
+
+
+
+                    }
 
                     if (existingEmployes.Count != 0)
                     {
                         for (int i = 0; i < existingEmployes.Count; i++)
                         {
-                            //if (existingEmployes[i]["vcChangeStatus"].ToString() != "Видалено")
-                            //{
+                            if (existingEmployes[i]["vcChangeStatus"].ToString() != "Видалено")
+                            {
                                 existingEmployes[i]["vcChangeStatus"] = "Видалено";
                                 existingEmployes[i]["vcChangeDate"] = DateTime.Now;
 
                                 NLogCore.LogStatusAplication("Запис з ідентифікатором eEmployees1Id=" + existingEmployes[i]["eEmployees1Id"].ToString() + " видалено");
-                           // }
+                            }
                         }
                     }
 
@@ -178,9 +240,10 @@ namespace DataBaseMigrator.Infrastructure
                 }
             }
         }
+
         protected void DeleteCheck(ref DataTable campus, ref DataTable Vkd)
         {
-            var employeesId = campus.AsEnumerable().Select(o=>new
+            var employeesId = campus.AsEnumerable().Select(o => new
             {
                 EmpId = o["ID_employee"],
                 SubId = o["ID_Subdivision"]
@@ -213,28 +276,28 @@ namespace DataBaseMigrator.Infrastructure
         #region DataBaseConventorHelper
         private void RowUpdate(DataRow oldRow, DataRow newRow)
         {
-           List<string> columns = new List<string>();
-           List<string> newVals = new List<string>();
-           bool flag = false;
-           foreach (var columname in CountNameColumn.NameColumn)
-           {
-               flag |= CellUpdate(ref oldRow, newRow, ref columns, ref newVals, columname);
-           }
-               
-           if (flag)
-           {
-               oldRow["UserAccountId"] = GetUserAccountID(newRow); 
-               oldRow["vcChangeDate"] = DateTime.Now;
-               oldRow["vcChangeStatus"] = "Оновлено";
-               for (int i = 0; i < newVals.Count; i++)
-               {
-                  NLogCore.LogStatusAplication("Запис з ідентифікатором eEmployees1Id=" +
-                                               oldRow["eEmployees1Id"].ToString() +
-                                               " змінено: значення поля " +
-                                               columns[i].ToString() + " замінено на " +
-                                               newVals[i].ToString());
-                }                    
-           }      
+            List<string> columns = new List<string>();
+            List<string> newVals = new List<string>();
+            bool flag = false;
+            foreach (var columname in CountNameColumn.NameColumn)
+            {
+                flag |= CellUpdate(ref oldRow, newRow, ref columns, ref newVals, columname);
+            }
+
+            if (flag)
+            {
+                oldRow["UserAccountId"] = GetUserAccountID(newRow);
+                oldRow["vcChangeDate"] = DateTime.Now;
+                oldRow["vcChangeStatus"] = "Оновлено";
+                for (int i = 0; i < newVals.Count; i++)
+                {
+                    NLogCore.LogStatusAplication("Запис з ідентифікатором eEmployees1Id=" +
+                                                 oldRow["eEmployees1Id"].ToString() +
+                                                 " змінено: значення поля " +
+                                                 columns[i].ToString() + " замінено на " +
+                                                 newVals[i].ToString());
+                }
+            }
         }
         private bool CellUpdate(ref DataRow oldRow, DataRow newRow, ref List<string> columns, ref List<string> newVals, string colName)
         {
@@ -274,22 +337,22 @@ namespace DataBaseMigrator.Infrastructure
         }
         private void CloneRow(DataRow sourceRow, int employeesId, ref DataRow newRow)
         {
-                newRow["eEmployees1Id"] = employeesId;
-                foreach (var columname in CountNameColumn.NameColumn)
+            newRow["eEmployees1Id"] = employeesId;
+            foreach (var columname in CountNameColumn.NameColumn)
+            {
+                try
                 {
-                    try
-                    {
-                        newRow[columname] = sourceRow[columname];
-                    }
-                    catch
-                    {
-                        newRow["IdRtStaffHoliday"] = sourceRow["IdStaffHolidays"];
-                    }
+                    newRow[columname] = sourceRow[columname];
                 }
-                newRow["UserAccountId"] = GetUserAccountID(sourceRow);
-                newRow["vcChangeDate"] = DateTime.Now;
-                newRow["vcChangeStatus"] = "Створено";
-            
+                catch
+                {
+                    newRow["IdRtStaffHoliday"] = sourceRow["IdStaffHolidays"];
+                }
+            }
+            newRow["UserAccountId"] = GetUserAccountID(sourceRow);
+            newRow["vcChangeDate"] = DateTime.Now;
+            newRow["vcChangeStatus"] = "Створено";
+
         }
 
         private object GetUserAccountID(DataRow currentrow)
@@ -310,7 +373,7 @@ namespace DataBaseMigrator.Infrastructure
                     result.Read();
                     if (result["UserAccountId"] != DBNull.Value)
                     {
-                        userid =  result.GetInt32(0);
+                        userid = result.GetInt32(0);
                     }
                     else
                     {
